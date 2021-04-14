3d97c41b01d804fd67f27e64650661353c203847
Alekcandr-Shapovalov <69868548+Alekcandr-Shapovalov@users.noreply.github.com>
2021-04-14 21:55:15 +0300

    Add files via upload

---
+++ Server.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using SticksyProtocol;
+using AnswerMethods;
+
+namespace serverSticksy
+{
+    class Server
+    {
+        public TcpListener server { get; }
+        public AnswerMethods answerMethods;
+
+        public Server(IPEndPoint iP)
+        {
+            server = new TcpListener(iP);
+            answerMethods = new AnswerMethods(server);
+        }
+
+        public void Listen()
+        {
+            while (true)
+            {
+                try {
+                    TcpClient client = server.AcceptTcpClient();
+                    ListenClientAsync(client);
+                }
+                catch { }
+            }
+        }
+
+        private async void ListenClientAsync(TcpClient client)
+        {
+            await Task.Run(() =>
+            {
+                while (true)
+                {
+                    try
+                    {
+                        IData data = Transfer.ReceiveData(client);
+                        if (data is GetUsers)
+                        {
+                            answerMethods.getUsers(data as GetUsers);
+                        }
+                        else if (data is )
+                        {
+
+                        }
+                    }
+                    catch
+                    {}
+                }
+            });
+        }
+
+    }
+}
